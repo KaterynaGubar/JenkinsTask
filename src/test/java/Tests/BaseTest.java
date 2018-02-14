236e1fcf0e4bec6bf634ecd7eb9401134ef1e0c8
Kateryna Gubar <kateryna_gubar@epam.com>
2018-02-14 15:55:50 +0200

    driver

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,15 +1,23 @@
 package Tests;
 
 import com.codeborne.selenide.Configuration;
+import core.DriverFactory;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public abstract class BaseTest {
 
+    static RemoteWebDriver webDriver;
+
     @BeforeTest
-    public void init() {
+    public void init() throws MalformedURLException {
+        webDriver = DriverFactory.getDriver("chrome");
+        webDriver.manage().window().maximize();
 
         Configuration.browser = "chrome";
     }
@@ -18,5 +26,4 @@ public abstract class BaseTest {
     public void openMainPage() {
         open("https://pn.com.ua/");
     }
-
 }
